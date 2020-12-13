da1bdc4d4cc90aeb6c371815c44e6c78d67f9e7d
Sho372 <32non23ca@gmail.com>
2020-12-14 02:05:20 +0900

    add Exercises-Level1 #1

---
+++ code_samples/exercises/level1/1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// short declaration
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	// multiple print statements
+	fmt.Println(x, y, z)
+
+}
