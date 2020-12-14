fb10c07f5bc41170136974239b7c68f02cab16c5
Sho372 <32non23ca@gmail.com>
2020-12-15 06:59:31 +0900

    Level2 #1 a number in decimal, binary and hex

--- code_samples/exercises/level1/4/main.go
+++ code_samples/exercises/level1/4/main.go
@@ -9,14 +9,17 @@ type weight int //underlying type is int
 type (
 	age    int   //Type defnition. underlying type is int
 	height = int //Alias declarations. underlying type is int
-
 )
 
 var x age
+var y int
 
 func main() {
 	fmt.Printf("%T\n", x)
 	fmt.Printf("My age is %d\n", x) // ZERO value -> 0
 	x = 42
 	fmt.Printf("My age is %d", x)
+	y = int(x) // conversion type age -> type int
+	fmt.Printf("Value of y is %d",y)
+	fmt.Printf("Type of y is %T",y)
 }
---
+++ code_samples/exercises/level2/1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	//Print decimal, binary and hex
+	fmt.Printf("%d\t%b\t%#x", x, x, x)
+}
