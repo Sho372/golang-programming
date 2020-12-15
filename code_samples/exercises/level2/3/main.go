50f36e8f2e0bfd649da9f7c08c6502ed6d015fb8
Sho372 <32non23ca@gmail.com>
2020-12-15 10:32:33 +0900

    Level2 #3 typed and untyped constant

---
+++ code_samples/exercises/level2/3/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const x = "hello" // untyped constant
+	const y string = "world" // typed constant
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Println(x, y)
+}
